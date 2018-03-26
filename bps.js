2cccbd04698bab465f7008da939c504371a46ed5
ansigroup <37701692+ansigroup@users.noreply.github.com>
2018-03-26 12:40:51 +0300

    Add files via upload

---
+++ bps.js
@@ -0,0 +1,47 @@
+/**********************************************
+*
+* CryptoLions.io
+* https://github.com/CryptoLions/EOS-Testnet-monitor
+*
+***********************************************/
+
+
+var blockProducerList = [
+  {
+    "bp_name": "lion",
+    "organisation": "CryptoLions.io",
+    "location": "Ukraine, Kyiv",
+    "node_addr": "jungle.cryptolions.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876"
+  },
+  {
+    "bp_name": "tiger",
+    "organisation": "CryptoLions.io",
+    "location": "Ukraine, Lviv",
+    "node_addr": "193.93.219.219",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876"
+  },
+  {
+    "bp_name": "jaguar",
+    "organisation": "EOSNet.io",
+    "location": "USA, Bluffdale, UT",
+    "node_addr": "eosslc.com",
+    "port_http": "8876",
+    "port_ssl": "",
+    "port_p2p": "9876"
+  },
+  {
+    "bp_name": "python",
+    "organisation": "Prometeos.io",
+    "location": "Spain",
+    "node_addr": "python.prometeos.io",
+    "port_http": "8888",
+    "port_ssl": "",
+    "port_p2p": "9876"
+  }
+
+]
